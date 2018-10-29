1fed06fdeaca351f8992a1b194d47cc9ff082989
gdack <arvinhuz@gmail.com>
2018-10-29 21:52:36 +0800

    Add: write a citation mapreduce program.

---
+++ src/main/java/com/gdack/hadoop/v1/CitationTemplate.java
@@ -0,0 +1,72 @@
+package com.gdack.hadoop.v1;
+
+import java.io.IOException;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+public class CitationTemplate {
+
+  public static class MapClass extends Mapper<Text, Text, Text, Text> {
+
+    @Override
+    protected void map(Text key, Text value, Context context)
+        throws IOException, InterruptedException {
+      super.map(key, value, context);
+    }
+  }
+
+  public static class Reduce extends Reducer<Text, Text, Text, Text> {
+
+    @Override
+    protected void reduce(Text key, Iterable<Text> values, Context context)
+        throws IOException, InterruptedException {
+      StringBuilder csv = new StringBuilder();
+      for (Text value : values) {
+        if (csv.length() > 0) {
+          csv.append(",");
+        }
+        csv.append(value.toString());
+      }
+
+      context.write(key, new Text(csv.toString()));
+    }
+  }
+
+  public static void main(String[] args)
+      throws IOException, ClassNotFoundException, InterruptedException {
+    Configuration conf = new Configuration();
+    conf.set("mapreduce.input.keyvaluelinerecordreader.key.value.separator", ",");
+    String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+
+    if (otherArgs.length < 2) {
+      System.err.println("Usage: citation <in> [<in>...] <out>");
+      System.exit(2);
+    }
+
+    Job job = Job.getInstance(conf, "citation");
+    job.setJarByClass(CitationTemplate.class);
+    job.setMapperClass(MapClass.class);
+    job.setCombinerClass(Reduce.class);
+    job.setReducerClass(Reduce.class);
+    job.setInputFormatClass(KeyValueTextInputFormat.class);
+    job.setOutputValueClass(TextOutputFormat.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(Text.class);
+
+    for (int i = 0; i < otherArgs.length - 1; i++) {
+      FileInputFormat.addInputPath(job, new Path(otherArgs[i]));
+    }
+    FileOutputFormat.setOutputPath(job, new Path(otherArgs[otherArgs.length - 1]));
+
+    System.exit(job.waitForCompletion(true)? 0 : 1);
+  }
+}
